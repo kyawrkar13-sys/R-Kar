2b27e75204a8195c0f1e8010fb221a0be2d13465
kyawrkar13-sys <kyawrkar13@gmail.com>
2026-07-22 05:30:43 +0630

    Update roof.js

--- roof.js
+++ roof.js
@@ -1,1 +1,64 @@
+[ 22 July 2026 05:29 ] ⁨Rkar Kyaw⁩: function roofLayout(){
 
+let roofL = parseFloat(document.getElementById("roofLength").value);
+let roofW = parseFloat(document.getElementById("roofWidth").value);
+
+let panelL = parseFloat(document.getElementById("panelLength").value) / 1000;
+let panelW = parseFloat(document.getElementById("panelWidth").value) / 1000;
+
+let walkway = parseFloat(document.getElementById("walkway").value);
+let setback = parseFloat(document.getElementById("setback").value);
+
+
+// usable roof area
+let usableL = roofL - (setback * 2);
+let usableW = roofW - (setback * 2) - walkway;
+
+
+// Portrait calculation
+let pCols = Math.floor(usableL / panelW);
+let pRows = Math.floor(usableW / panelL);
+
+let portrait = pCols * pRows;
+
+
+// Landscape calculation
+let lCols = Math.floor(usableL / panelL);
+let lRows = Math.floor(usableW / panelW);
+
+let landscape = lCols * lRows;
+
+
+// kWp (650W example)
+let portraitKW = portrait * 650 / 1000;
+let landscapeKW = landscape * 650 / 1000;
+
+
+let recommend;
+
+if(landscape > portrait){
+recommend="Landscape";
+}
+else if(portrait > landscape){
+recommend="Portrait";
+}
+else{
+recommend="Same";
+}
+
+
+document.getElementById("roofResult").innerHTML=
+
+`
+Roof Area : ${(roofL*roofW).toFixed(2)} m²<br>
+Usable Length : ${usableL.toFixed(2)} m<br>
+Usable Width : ${usableW.toFixed(2)} m<br><br>
+
+Portrait : ${portrait} Panels (${portraitKW.toFixed(2)} kWp)<br>
+
+Landscape : ${landscape} Panels (${landscapeKW.toFixed(2)} kWp)<br><br>
+
+⭐ Recommended : ${recommend}
+`;
+
+}
